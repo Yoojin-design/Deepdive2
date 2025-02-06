d8b210b39593556cf98cbb646b9213ce148fdb64
Yoojin-design <jennifer606@naver.com>
2025-02-06 22:45:32 +0900

    Include button input textarea html files

--- js/global.js
+++ js/global.js
@@ -3,3 +3,6 @@ import { includeHTML } from "./include.js";
 includeHTML("guide/heading.html", "#heading");
 includeHTML("guide/text.html", "#text");
 includeHTML("guide/link.html", "#link");
+includeHTML("guide/button.html", "#button");
+includeHTML("guide/input.html", "#input");
+includeHTML("guide/textarea.html", "#textarea");
