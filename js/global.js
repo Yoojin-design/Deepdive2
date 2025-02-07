87b47213ec0b4b3556bc049ea835d0343f1d2dfd
de <daunii2544@gmail.com>
2025-02-07 19:09:26 +0900

    textarea, select

---
+++ js/global.js
@@ -0,0 +1,9 @@
+import { includeHTML } from "./include.js";
+
+includeHTML("/guide/heading.html", "#heading");
+includeHTML("/guide/txt.html", "#txt");
+includeHTML("/guide/link.html", "#link");
+includeHTML("/guide/button.html", "#button");
+includeHTML("/guide/input.html", "#input");
+includeHTML("/guide/textarea.html", "#textarea");
+includeHTML("/guide/select.html", "#select");
