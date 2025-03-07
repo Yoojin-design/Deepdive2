116110f434c33172b64891c0a1a2cefc17327b77
de <daunii2544@gmail.com>
2025-03-07 21:40:46 +0900

    next button

--- js/DE.js
+++ js/DE.js
@@ -13,6 +13,34 @@ gsap.fromTo(
 );
 
 //Onboarding01//
+
+gsap
+  .timeline()
+  .from(".onboarding-body", {
+    // opacity: 0,
+    // y: 70,
+    width: 0,
+    duration: 1,
+    ease: "power3.inOut",
+  })
+  .from(
+    ".stepper",
+    {
+      scale: 1.2,
+      opacity: 0,
+    },
+    ">"
+    // "-=0.5"
+  )
+  .from(
+    ".title_v2",
+    {
+      opacity: 0,
+      stagger: 0.3,
+    },
+    "-=0.5"
+  );
+
 gsap.from(".stepper img", { y: 100, opacity: 0, duration: 1, stagger: 0.2, ease: "back. inOut" });
 
 gsap.from(".xchip label", {
@@ -24,3 +52,20 @@ gsap.from(".xchip label", {
     from: "random",
   },
 });
+
+const activeNext = () => {
+  const checkbox = document.querySelectorAll(".xchip input");
+  checkbox.forEach((chk) => {
+    chk.addEventListener("click", (e) => {
+      if (e.target.checked) {
+        document.querySelector(".btn-text").classList.add("btn-primary");
+        document.querySelector(".btn-text").classList.remove("btn-text");
+      } else {
+        document.querySelector(".btn-primary").classList.add("btn-text");
+        document.querySelector(".btn-text").classList.remove("btn-primary");
+      }
+    });
+  });
+};
+
+activeNext();
