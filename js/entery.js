1faa73257f1f469a4ac756c20b0ebcb8eb82ef48
huijeong-sim <huijeong9331@gmail.com>
2025-03-11 21:50:40 +0900

    a

--- js/entery.js
+++ js/entery.js
@@ -1,10 +1,28 @@
-gsap.timeline({}).fromTo(
-  ".product-scroll li",
-  {
-    opacity: 0,
-  },
-  {
-    opacity: 1,
-    stagger: 0.2,
-  }
-);
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".product-columns",
+      marker: true,
+      start: "top center",
+    },
+  })
+  .fromTo(
+    ".product-scroll li",
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 1,
+      stagger: 0.2,
+    }
+  )
+  .fromTo(
+    ".product-columns li",
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 10,
+      stagger: 0.2,
+    }
+  );
